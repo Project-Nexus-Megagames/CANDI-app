9a69957de612e7da5d9b4b43820c59214572d950
SMcMann <smcmann42@gmail.com>
2021-01-24 22:45:26 -0800

    update version

--- reactapp/src/components/navigation/homepage.jsx
+++ reactapp/src/components/navigation/homepage.jsx
@@ -26,7 +26,7 @@ class HomePage extends Component {
 						<span> {this.props.gamestate.round} </span>
 					</div>
 					<div > <b>Game Version:</b>
-						<span> 1.02</span>
+						<span> 1.03</span>
 					</div>
 				</Content>
 			<Footer>
