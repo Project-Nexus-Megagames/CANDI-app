943751c968eb70fc910a61acd9d91b4cfbbdccaa
SMcMann <smcmann42@gmail.com>
2021-02-01 00:17:10 -0800

    extra spook

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
           <Content>
             <FlexboxGrid justify="center">
               <FlexboxGrid.Item key={1} colspan={12} style={{marginTop: '80px'}}>
-                <img src={'https://media4.giphy.com/media/tJMVqwkdUIuL0Eiam3/source.gif'} alt={'Loading...'} />  
+                <img src={spook[(Math.floor(Math.random() * spook.length ))]} alt={'Loading...'} />  
               </FlexboxGrid.Item>
             </FlexboxGrid>
           </Content> <b>Loading...</b>
@@ -223,5 +223,32 @@ const model = Schema.Model({
   email: StringType()
     .isRequired('This field is required.')
 });
+
+const spook = [
+  'https://media4.giphy.com/media/tJMVqwkdUIuL0Eiam3/source.gif',
+  'https://media2.giphy.com/media/l0HlCkojKEiPhw86Y/giphy.gif',
+  'https://media2.giphy.com/media/65Mt9P22p2zMzpDPZ7/giphy.gif',
+  'https://media4.giphy.com/media/3o7TKpmHsAZiTTekve/giphy.gif',
+  'https://media1.giphy.com/media/5DV5DkiGIlAxq/giphy.gif',
+  'https://media1.giphy.com/media/12mbWQRCFxOzp6/giphy.gif',
+  'https://media1.giphy.com/media/UrljInRDir27u/giphy.gif',
+  'https://media4.giphy.com/media/b04Dkxq5AUOCA/giphy.gif',
+  'https://media0.giphy.com/media/9V8Zkw4N7wef4wVTMj/giphy.gif',
+  'https://media0.giphy.com/media/3o7TKFiWSXV7JjjLFu/giphy.gif',
+  'https://media0.giphy.com/media/26BRCc2VNkdZ5tjvG/giphy.gif',
+  'https://media4.giphy.com/media/QBkBsqCs1fzB6lWtAg/giphy.gif',
+  'https://media3.giphy.com/media/1wqpNgYn6Ioi5KFpYu/giphy.gif',
+  'https://media3.giphy.com/media/NReptqJMy4AEqVBMLy/giphy.gif',
+  'https://media3.giphy.com/media/dAROqk8Rj2hijvQSZ6/giphy.gif',
+  'https://media2.giphy.com/media/J3FG7OCZ41ECEGg5xK/giphy.gif',
+  'https://media0.giphy.com/media/26BRxmqeqsRPBBOpy/giphy.gif',
+  'https://media3.giphy.com/media/1NTrtg7jDz8XwSObFE/giphy.gif',
+  'https://media1.giphy.com/media/623LlMM8HuzF2gD12N/giphy.gif',
+  'https://media0.giphy.com/media/WFk0kxBWUgDjgmN2G9/giphy.gif',
+  'https://media4.giphy.com/media/26xBtwZMRHvhP4WAM/giphy.gif',
+  'https://media3.giphy.com/media/dt0T8TI3Kizyctrxh9/giphy.gif',
+  'https://media4.giphy.com/media/kbnUIUVY7YHNhe5tbb/giphy.gif'
+
+]
 
 export default (App);
