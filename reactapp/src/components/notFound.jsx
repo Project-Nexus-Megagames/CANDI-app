63b14c68bb8b44846153138b57128b7fa3752eb9
SMcMann <smcmann42@gmail.com>
2021-02-25 15:50:40 -0800

    fix a thing

--- reactapp/src/components/notFound.jsx
+++ reactapp/src/components/notFound.jsx
@@ -4,15 +4,14 @@ import { useHistory } from "react-router-dom";
 
 const NotFound = () => {
   const [seconds, setSeconds] = React.useState(25);
+  const [startBool, setStart] = React.useState(false);
 	const history = useHistory()
 
-  useEffect(() => {
-    if (seconds <= 26) {
-      const audio = new Audio('/fanfare.mp3');
-      audio.loop = false;
-      audio.play();      
-    }
-
+  const start = () => {
+    setStart(startBool => startBool = true);
+    const audio = new Audio('/fanfare.mp3');
+    audio.loop = false;
+    audio.play();  
 
     const interval = setInterval(() => {
       if (seconds > 1300) {
@@ -22,14 +21,16 @@ const NotFound = () => {
         setSeconds(seconds => seconds + 2);
     }, 50);
     return () => clearInterval(interval);
-  }, [seconds]);
+  }
 
   
   return ( 
 		<React.Fragment>
-      <Header>
-      </Header>
-      <Content style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+
+      {!startBool && <Header style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+        <Button appearance={'ghost'} onClick={()=> start()}>Click Me</Button>
+      </Header>}
+      {startBool && <Content style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
         <svg xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" width={seconds > 1050 ? 1050: seconds} viewBox="0 0 1211 495">
           <defs>
          </defs>
@@ -38,8 +39,8 @@ const NotFound = () => {
        {seconds >= 1100 && <div style={{ fontSize: '34px', fontWeight: '500',	color: '#fff', lineHeight: '1.4',	margin: '0'}}>Looks like something went wrong.</div>}
        {seconds >= 1200 && <Button color='red' size='lg' onClick={()=> history.push('/')} >Take me back!</Button>}
        {seconds >= 1300 && <p>Also I really hope you had audio on...</p> }
-      </Content>
-         </React.Fragment>
+      </Content>}
+    </React.Fragment>
 	)
 }
  
