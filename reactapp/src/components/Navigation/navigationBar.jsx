b3ae1d39a0df92421fb3fb176b037c6bb50bd875
SMcMann <57707915+SMcMann@users.noreply.github.com>
2021-04-05 14:59:31 -0700

    Update navigationBar.jsx

--- reactapp/src/components/Navigation/navigationBar.jsx
+++ reactapp/src/components/Navigation/navigationBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Navbar, Nav, Icon, Whisper, Tooltip} from 'rsuite';
+import { Navbar, Nav, Icon, Tooltip} from 'rsuite';
 import { connect } from "react-redux";
 import { signOut } from '../../redux/entities/auth';
 class Navigation extends Component {
@@ -12,7 +12,7 @@ class Navigation extends Component {
 	
 	componentDidMount() {
 		this.renderTime(this.props.gamestate.endTime);
-		const interval = setInterval(() => {
+		setInterval(() => {
 			this.renderTime(this.props.gamestate.endTime);
         //clearInterval(interval);
     }, 60000);
@@ -24,18 +24,10 @@ class Navigation extends Component {
 				<Navbar >
 				<Navbar.Body>
 					<Nav onSelect={this.props.onSelect} activeKey={this.props.active}>
-						<Whisper placement="bottom" trigger="hover" speaker={tooltip}><Nav.Item eventKey="login" icon={<Icon icon="sign-out"/>} onClick={()=> this.props.logOut()}></Nav.Item></Whisper> 
-						<Nav.Item eventKey="home" to="/home" componentClass={NavLink} icon={<Icon icon="home" />}>Home</Nav.Item>
-						<Nav.Item eventKey="character" to="/character" componentClass={NavLink} >My Character</Nav.Item>
-						<Nav.Item eventKey="actions" to="/actions" componentClass={NavLink} >Actions</Nav.Item>
-						<Nav.Item eventKey="others" to="/others" componentClass={NavLink} >Other Characters</Nav.Item>
-						<Nav.Item eventKey="controllers" to="/controllers" componentClass={NavLink} >Control Team</Nav.Item>
-						{this.props.user.roles.some(el=> el === 'Control') && <Nav.Item eventKey="control" to="/control" componentClass={NavLink} style={{backgroundColor: "#61342e"}}>Control Terminal</Nav.Item>}
-						{/*this.props.user.roles.some(el=> el === 'Control') && <Nav.Item eventKey="reg" to="/reg" componentClass={NavLink} style={{backgroundColor: "#61342e"}}>User Registration</Nav.Item>*/}
-					</Nav>			
-
+					<Nav.Item eventKey="home" to="/home" componentClass={NavLink} icon={<Icon icon="arrow-circle-left" />}>Back</Nav.Item>
+				</Nav>		
 				</Navbar.Body>			
-				<div style={{ position: 'fixed', top: 5, right: 25  }}>
+				<div style={{  }}>
 					<p style={{ }}  >Round: {this.props.gamestate.round} </p>	
 					{(this.state.days > 0) && <p>Time Left: {days} Days, {hours} Hours </p>}
 					{(this.state.hours > 0 && this.state.days <= 0) && <p>Time Left: {hours} Hours, {minutes} Minutes</p>}	
@@ -47,7 +39,8 @@ class Navigation extends Component {
 		}
 
 		handleLogOut = async () => {
-			localStorage.removeItem('token');
+			this.props.logOut()
+			//localStorage.removeItem('token');
 			//props.history.push('/login')
 		}
 		
@@ -76,12 +69,6 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
 	logOut: () => dispatch(signOut())
 });
-
-const tooltip = (
-  <Tooltip>
-    Log-Out
-  </Tooltip>
-);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
 
