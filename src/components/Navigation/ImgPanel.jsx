ffc2513f2a1165eb447ed93a2d8aefca7404644d
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-05-17 18:55:52 -0700

    ADDED - Dynamic ImgPanel Disabling

--- src/components/Navigation/ImgPanel.jsx
+++ src/components/Navigation/ImgPanel.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 const ImgPanel = (props) => {
     return (
         <Panel style={{width: props.width, height: props.height, position: 'relative', float:'left', display:'inline-block', margin: '10px'}} shaded bodyFill>
-            <Link to={props.to}>
+            <Link to={props.disabled ? '#' : props.to}>
             <div className="container">
-                <img src={props.img} className='image' height='auto' alt='Failed to load img' />             
+                <img src={props.img} className={props.disabled ? 'image disabled' : 'image'} height='auto' alt='Failed to load img' />             
             </div>
             </Link>
-            <h6 style={{position: 'absolute', bottom: '25px', left: '15px', color:'white'}}>{props.title}</h6>
+            <h6 style={{position: 'absolute', bottom: '25px', left: '15px', color:'white'}}>{`${props.disabled ? '[DISABLED] ' : ''}${props.title}`}</h6>
             <p style={{position: 'absolute', bottom: '10px', left: '15px', color:'white'}}>{props.body}</p>
         </Panel>
     );
