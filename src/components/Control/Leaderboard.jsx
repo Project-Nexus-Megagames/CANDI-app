52df7dd4d9b7214f8641fe986f8747930b1789a5
SMcMann <smcmann42@gmail.com>
2021-11-02 15:27:36 -0700

    fix leaderboard

--- src/components/Control/Leaderboard.jsx
+++ src/components/Control/Leaderboard.jsx
@@ -7,29 +7,29 @@ import { getGodBonds, getMortalBonds } from '../../redux/entities/assets';
 
 const  Leaderboard = (props) => {
 	const [copy, setCopy] = React.useState([]);
+	const [number, setNumber] = React.useState(3);
 
 	useEffect(() => {
 		let array = [];
 		const bonds = [ ...props.godBonds, ...props.mortalBonds];
 		for (const char of props.characters.filter(el => el.tags.some(tag => tag === 'PC'))) {
-			// console.log(char.characterName)
+			console.log(char.characterName)
 			let charBonds = bonds.filter(bond => bond.ownerCharacter === char._id );
 
 			// console.log('===Justice===')
-			const Justice = calculate(charBonds, ['Zeus', 'Hera']); 
-			const Trickery = calculate(charBonds, ['Hermese']);
+			const Justice = calculate(charBonds, ['Hera']); 
+			const Trickery = calculate(charBonds, ['Hermes']);
 			const Balance = calculate(charBonds, ['Demeter']);
 			const Hedonism = calculate(charBonds, ['Dionysus']);
 			const Bonding = calculate(charBonds, ['Aphrodite']);
 			const Arts = calculate(charBonds, ['Apollo']);
 			const Sporting = calculate(charBonds, ['Artemis']);
 			const Fabrication = calculate(charBonds, ['Hephaestus']);
-			// console.log('===Scholarship===')
 			const Scholarship = calculate(charBonds, ['Athena']);
-			const Pugilism = calculate(charBonds, ['Hephaestus']);
-			// const Glory = calculate(charBonds, ['Zeus', 'Hades', 'Poseiden']);
+			const Pugilism = calculate(charBonds, ['Ares'], char.characterName);
+			// const Glory = calculate(charBonds, ['Zeus', 'Hades', 'Poseidon']);
 
-			// console.log(Scholarship)
+			console.log(char.Pugilism)
 			// console.log(Scholarship + char.Scholarship)
 			const character = {
 				characterName: char.characterName,
@@ -66,7 +66,7 @@ const  Leaderboard = (props) => {
 
 	const calculate = (charBonds, gods, show) => {
 		let array = charBonds.filter(bond => gods.some(god => god === bond.with.characterName));
-		if (show && show === 'Martin Keene') {
+		if (show && show === 'Chandra L. Aurelian') {
 			console.log(show)
 			console.log(`Preferred: + ${(array.filter(el => el.level=== 'Preferred').length)}`);
 			console.log(`Favoured + ${(array.filter(el => el.level === 'Favoured').length * 3)}`);
@@ -96,13 +96,14 @@ const  Leaderboard = (props) => {
 		<React.Fragment>
 			<NavigationBar/>
 				<h3>Leaderboard</h3>
+				{props.myCharacter.tags.some(el => el === 'Control') && <Button onClick={() => setNumber(100)}>Show All</Button>}
         <Grid fluid>
           <Row>
             <Col xs={24} sm={24} md={12} className="gridbox">
               <div style={{ border: '3px solid gray', borderRadius: '5px', width: '100%', }} >
 							<h5 style={{ backgroundColor: 'gray' }}>Justice</h5>
 							<List hover size="sm">
-								{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Justice - a.Justice}).splice(0, 3).map((character, index) => (
+								{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Justice - a.Justice}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -127,7 +128,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid Yellow', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: 'yellow', color: 'black' }}>Trickery</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Trickery - a.Trickery}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Trickery - a.Trickery}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -152,7 +153,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid #22a12a', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: '#22a12a' }}>Balance</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Balance - a.Balance}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Balance - a.Balance}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -177,7 +178,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid Purple', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: 'Purple' }}>Hedonism</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Hedonism - a.Hedonism}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Hedonism - a.Hedonism}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -202,7 +203,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid  #0f8095', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: ' #0f8095' }}>Bonding</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Bonding - a.Bonding}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Bonding - a.Bonding}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -227,7 +228,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid Brown', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: 'Brown' }}>Arts</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Arts - a.Arts}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Arts - a.Arts}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -252,7 +253,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid #950f80', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: '#950f80' }}>Sporting</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Sporting - a.Sporting}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Sporting - a.Sporting}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -277,7 +278,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid Orange', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: 'Orange' }}>Fabrication</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Fabrication - a.Fabrication}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Fabrication - a.Fabrication}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -288,7 +289,7 @@ const  Leaderboard = (props) => {
 										</FlexboxGrid.Item>
 										<FlexboxGrid.Item colspan={2}>
 											{props.myCharacter.tags.some(el => el === 'Control') && <div>
-												{character.Sporting} ({character.originalSporting})
+												{character.Fabrication} ({character.originalFabrication})
 											</div>}
 										</FlexboxGrid.Item>
 									</FlexboxGrid>
@@ -302,7 +303,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid #4169e1', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: '#4169e1', color: 'black' }}>Scholarship</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Scholarship - a.Scholarship}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Scholarship - a.Scholarship}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 									<FlexboxGrid>
 										<FlexboxGrid.Item colspan={2}>
@@ -327,7 +328,7 @@ const  Leaderboard = (props) => {
 							<div style={{ border: '3px solid #8a0303', borderRadius: '5px', width: '100%', }} >
 								<h5 style={{ backgroundColor: '#8a0303' }}>Pugilism</h5>
 								<List hover size="sm">
-									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Pugilism - a.Pugilism}).splice(0, 3).map((character, index) => (
+									{copy.filter(el => el.tags.some(tag => tag === 'PC')).sort(function(a, b){return b.Pugilism - a.Pugilism}).splice(0, number).map((character, index) => (
 									<List.Item key={index} index={index}>
 										<FlexboxGrid>
 											<FlexboxGrid.Item colspan={2}>
@@ -338,7 +339,7 @@ const  Leaderboard = (props) => {
 											</FlexboxGrid.Item>
 											<FlexboxGrid.Item colspan={2}>
 												{props.myCharacter.tags.some(el => el === 'Control') && <div>
-												{character.Sporting} ({character.originalSporting})
+												{character.Pugilism} ({character.originalPugilism})
 											</div>}
 											</FlexboxGrid.Item>
 										</FlexboxGrid>
