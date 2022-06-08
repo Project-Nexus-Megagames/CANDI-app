3d52c6092bf3997f09cd300015742df5ea951737
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-06-09 01:19:07 +0200

    linting errors again

--- src/components/Map/HexMap.jsx
+++ src/components/Map/HexMap.jsx
@@ -1,84 +1,82 @@
-
 import { connect } from 'react-redux';
 import React, { useEffect } from 'react';
 
 const HexMap = (props) => {
-	const [hexagons, setHexagons] = React.useState([]); 
+	const createHex = () => {
+		console.log('Rendering Hex Map...');
+		const container = document.querySelector('.container');
+		const hexagonPattern = [5, 6, 5, 4, 3, 2];
 
-  const createHex = () => {
-    console.log('Rendering Hex Map...')
-    const container = document.querySelector('.container');
-    const hexagonPattern = [5, 6, 5, 4, 3, 2];
-    
-    for (let i = 0; i < hexagonPattern.length; i++) {
-      const row = document.createElement('div');
-      row.classList.add('row');
-       for (let j = 0; j < hexagonPattern[i]; j++) {
-        const loc = props.locations.find(el => el.coords.x === i && el.coords.y === j);
-        const hexagon = document.createElement('div');
-        hexagon.classList.add('hexagon');
-        hexagon.classList.add('container');
-        hexagon.key = getHexId(i, j)
-        hexagon.id =  `${i}${j}`
-        hexagon.style.backgroundImage = loc ? 
-         `url(/images/map/${getHexId(i, j)}.jpg)` :
-         'url("/images/map/fog.jpg")'
-        const text = document.createElement('div')
-        !loc ? text.classList.add('center') : text.classList.add('dicovered-text') ;
-        text.id =  `${i}${j}`
-        text.innerHTML = getHexId(i, j);
-        
-        hexagon.appendChild(text);
-  
-        row.appendChild(hexagon);
-        hexagon.addEventListener("mouseenter", hoverChange);
-        hexagon.addEventListener("mouseout", hoverOut);
-        text.addEventListener("mouseenter", hoverChange);
-        }
-        container.appendChild(row);
-  }
-  }
-  
-  const getHexId = (row, col) => {
-    const Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var letterIndex = row;
-    var letters = "";
-    while(letterIndex > 25)
-    {
-      letters = [letterIndex%26] + letters;
-      letterIndex -= 26;
-    }
-    return `${Letters[letterIndex] + letters + (col + 1)}`;
-  };
-  
-  function hoverChange(data) {
-    let div = document.getElementById(data.target.id);
-    // div.style.border= "url(" + "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png" + ")"
-    props.handleHover(data.target.id)
-  }
-  
-  function hoverOut(data) {
-    let div = document.getElementById(data.target.id);
-  }
+		for (let i = 0; i < hexagonPattern.length; i++) {
+			const row = document.createElement('div');
+			row.classList.add('row');
+			for (let j = 0; j < hexagonPattern[i]; j++) {
+				const loc = props.locations.find(
+					(el) => el.coords.x === i && el.coords.y === j
+				);
+				const hexagon = document.createElement('div');
+				hexagon.classList.add('hexagon');
+				hexagon.classList.add('container');
+				hexagon.key = getHexId(i, j);
+				hexagon.id = `${i}${j}`;
+				hexagon.style.backgroundImage = loc
+					? `url(/images/map/${getHexId(i, j)}.jpg)`
+					: 'url("/images/map/fog.jpg")';
+				const text = document.createElement('div');
+				!loc
+					? text.classList.add('center')
+					: text.classList.add('dicovered-text');
+				text.id = `${i}${j}`;
+				text.innerHTML = getHexId(i, j);
 
-  useEffect(() => {
-    createHex()
+				hexagon.appendChild(text);
+
+				row.appendChild(hexagon);
+				hexagon.addEventListener('mouseenter', hoverChange);
+				hexagon.addEventListener('mouseout', hoverOut);
+				text.addEventListener('mouseenter', hoverChange);
+			}
+			container.appendChild(row);
+		}
+	};
+
+	const getHexId = (row, col) => {
+		const Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		var letterIndex = row;
+		var letters = '';
+		while (letterIndex > 25) {
+			letters = [letterIndex % 26] + letters;
+			letterIndex -= 26;
+		}
+		return `${Letters[letterIndex] + letters + (col + 1)}`;
+	};
+
+	function hoverChange(data) {
+		//let div = document.getElementById(data.target.id);
+		// div.style.border= "url(" + "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg/1200px-Flag_of_Libya_%281977%E2%80%932011%2C_3-2%29.svg.png" + ")"
+		props.handleHover(data.target.id);
+	}
+
+	function hoverOut(data) {
+		//let div = document.getElementById(data.target.id);
+	}
+
+	useEffect(() => {
+		createHex();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-    return (
-      <div style={{   }}>
-        <h2>Tempest Map</h2>
-        <hr />
-        <div className="container">
-        </div>
-      </div>
-    );
-}
+	return (
+		<div style={{}}>
+			<h2>Tempest Map</h2>
+			<hr />
+			<div className="container"></div>
+		</div>
+	);
+};
 
-const mapStateToProps = (state) => ({
-});
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = (dispatch) => ({
-});
+const mapDispatchToProps = (dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(HexMap);
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,143 +1,160 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Container, Content, Icon, Loader, Sidebar, Divider, } from 'rsuite';
-import { useHistory } from "react-router-dom";
+import { Container, Content, Icon, Loader, Sidebar, Divider } from 'rsuite';
+import { useHistory } from 'react-router-dom';
 import HexMap from './HexMap';
-import socket from '../../socket';
 import { getMyLocations } from '../../redux/entities/locations';
 import NavigationBar from '../Navigation/NavigationBar';
 
-
 const Map = (props) => {
-	const { locations, login, loading, unlockedLocations, control, myCharacter, military } = props;
-	const defaultTerr = { name: 'Hover over territory to see details',  description: '?????',  currentOwner: '????????',  influence: 0, coords: { x: 0, y: 0 }}
-  const [value, setValue] = React.useState({ scale: 1, translation: { x: 0, y: 0 }});
-	const [territory, setTerritory] = React.useState(defaultTerr);
-	const [selected, setSelected] = React.useState(false);
-	const [tab, setTab] = React.useState('info');
-	const [mission, setMission] = React.useState(undefined); 
-  const [start, setStart] = React.useState();
+	const { locations, login, loading, unlockedLocations } = props;
+	const defaultTerr = {
+		name: 'Hover over territory to see details',
+		description: '?????',
+		currentOwner: '????????',
+		influence: 0,
+		coords: { x: 0, y: 0 }
+	};
+	const [value] = useState({
+		scale: 1,
+		translation: { x: 0, y: 0 }
+	});
+	const [territory, setTerritory] = useState(defaultTerr);
+	const [setSelected] = useState(false);
+	const [tab, setTab] = useState('info');
+	const [setMission] = useState(undefined);
+	const [setStart] = useState();
 
 	const history = useHistory();
-  
-	const width = 350// window.innerHeight / 2.5;
 
+	//const width = 350; // window.innerHeight / 2.5;
 
 	const clickHandlerer = (type, unit, coords) => {
-		console.log(type)
+		console.log(type);
 		switch (type) {
 			case 'Military':
 				setSelected(unit);
-				setStart(coords)
-				setTerritory(false)
+				setStart(coords);
+				setTerritory(false);
 				break;
 			case 'hex':
-				const loc = locations.find(el => el.coords.x === coords.q && el.coords.y === coords.r);
+				const loc = locations.find(
+					(el) => el.coords.x === coords.q && el.coords.y === coords.r
+				);
 				loc ? setTerritory(loc) : console.log('loc undefined');
 				break;
-			default: 
-				console.log(`${type} is BAD!!!`)
+			default:
+				console.log(`${type} is BAD!!!`);
 				break;
 		}
-	}
+	};
 
-  const handleIt = (value) => {
-    setValue(value)
-  }
-
-  const handleHover = (x) => {
-		const loc = locations.find(el => el.coords.x == x[0] && el.coords.y == x[1]);
+	const handleHover = (x) => {
+		const loc = locations.find(
+			// eslint-disable-next-line eqeqeq
+			(el) => el.coords.x == x[0] && el.coords.y == x[1]
+		);
 		if (loc) {
 			setTerritory(loc);
-		}
-		else {
+		} else {
 			setTerritory({
 				name: 'Undiscovered Territory',
 				description: 'Here be Monsters...',
 				currentOwner: value,
 				influence: -999,
 				coords: { x: parseInt(x[0]), y: parseInt(x[1]) }
-			})
+			});
 		}
-  }
+	};
 
 	const getHexId = (row, col) => {
-	const Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	var letterIndex = parseInt(row);
-	var letters = "";
-	while(letterIndex > 25)
-	{
-		letters = [letterIndex%26] + letters;
-		letterIndex -= 26;
-	}
-	
-	return `${Letters[letterIndex] + letters + (col + 1)}`;
-};
+		const Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		var letterIndex = parseInt(row);
+		var letters = '';
+		while (letterIndex > 25) {
+			letters = [letterIndex % 26] + letters;
+			letterIndex -= 26;
+		}
+
+		return `${Letters[letterIndex] + letters + (col + 1)}`;
+	};
 
 	const handleTab = (type) => {
 		setTab(type);
 		setMission(false);
-		setTerritory(defaultTerr)
-}
+		setTerritory(defaultTerr);
+	};
 
-  if (!login && !loading) {
-    history.push('/');
-    return (<Loader inverse center content="doot..." />)
-  };
-	return ( // 
-    <React.Fragment>
-			<NavigationBar/>
-      <Container  style={{ height: 'calc(100vh - 50px)',}}> 
-        <Content>
-          <div style={{ width: '100%', height: '100%' }}> 
-  					<HexMap handleHover={handleHover} handleClick={clickHandlerer} locations={unlockedLocations}/>     
-          </div>
-        </Content>
-			<Sidebar width={250} style={{transition: '0.8s ease'}}>
-								
-      <div
-        className="side-bar"
-        style={{
-          width: 250,
-          minHeight: '100vh'
-        }}
-      >
-				 <button
-          onClick={() => handleTab('info')}
-          className="toggle-menu"
-					style={{transform: `translate(${-20}px, ${100}px)`,	backgroundColor: '#087ad1' }}
-        ><Icon icon='info-circle' /></button>
+	if (!login && !loading) {
+		history.push('/');
+		return <Loader inverse center content="doot..." />;
+	}
+	return (
+		//
+		<React.Fragment>
+			<NavigationBar />
+			<Container style={{ height: 'calc(100vh - 50px)' }}>
+				<Content>
+					<div style={{ width: '100%', height: '100%' }}>
+						<HexMap
+							handleHover={handleHover}
+							handleClick={clickHandlerer}
+							locations={unlockedLocations}
+						/>
+					</div>
+				</Content>
+				<Sidebar width={250} style={{ transition: '0.8s ease' }}>
+					<div
+						className="side-bar"
+						style={{
+							width: 250,
+							minHeight: '100vh'
+						}}
+					>
+						<button
+							onClick={() => handleTab('info')}
+							className="toggle-menu"
+							style={{
+								transform: `translate(${-20}px, ${100}px)`,
+								backgroundColor: '#087ad1'
+							}}
+						>
+							<Icon icon="info-circle" />
+						</button>
 
-			{tab === 'info' && <div>
-				{unlockedLocations.length}
-				<h3>{territory.name}</h3>
-	  		<Divider>{territory.coords ? getHexId(territory.coords.x, territory.coords.y) : <b>Bad Coords</b>} - ({territory.coords.x}, {territory.coords.y}) </Divider>
-				<p className='p-left' >{territory.description}</p>
-				<Divider>Unlocked By</Divider>
-				{territory.unlockedBy && territory.unlockedBy.map(el => (
-					<div>{el.playerName}</div>
-					
-				))}
-			</div>}
-
-			</div>   
-			
-		</Sidebar>
-
-      </Container>      
-    </React.Fragment>
-
+						{tab === 'info' && (
+							<div>
+								{unlockedLocations.length}
+								<h3>{territory.name}</h3>
+								<Divider>
+									{territory.coords ? (
+										getHexId(territory.coords.x, territory.coords.y)
+									) : (
+										<b>Bad Coords</b>
+									)}{' '}
+									- ({territory.coords.x}, {territory.coords.y}){' '}
+								</Divider>
+								<p className="p-left">{territory.description}</p>
+								<Divider>Unlocked By</Divider>
+								{territory.unlockedBy &&
+									territory.unlockedBy.map((el) => <div>{el.playerName}</div>)}
+							</div>
+						)}
+					</div>
+				</Sidebar>
+			</Container>
+		</React.Fragment>
 	);
-}
+};
 
 const mapStateToProps = (state) => ({
 	unlockedLocations: state.auth.character ? getMyLocations(state) : [],
 	locations: state.locations.list,
 	control: state.auth.control,
 	characters: state.characters.list,
-  login: state.auth.login,
+	login: state.auth.login,
 	loading: state.auth.loading,
-  user: state.auth.user,
+	user: state.auth.user
 	// myCharacter: state.auth.user ? getMyCharacter(state): undefined
 });
 
