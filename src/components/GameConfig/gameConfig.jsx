eb23931aa6dc077fb4e99601f111586f1be206fd
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-07-01 01:04:16 +0200

    getting started on the form

--- src/components/GameConfig/gameConfig.jsx
+++ src/components/GameConfig/gameConfig.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux'; // Redux store provider
+import { useForm, useFieldArray } from 'react-hook-form';
 import {
-	Modal,
+	Input,
+	Form,
 	SelectPicker,
 	ButtonGroup,
 	Button,
@@ -11,20 +13,62 @@ import {
 } from 'rsuite';
 import socket from '../../socket';
 
-const GameConfig = () => {
-	const handleSubmit = () => {
-		const data = {};
-		try {
-			socket.emit('request', {
-				route: 'character',
-				action: 'healInjury',
-				data
-			});
-			// eslint-disable-next-line no-empty
-		} catch (err) {}
+function GameConfig() {
+	const { register, control, handleSubmit, reset, formState, setValue } =
+		useForm({
+			defaultValues: {
+				types: ['']
+			}
+		});
+	const { errors } = formState;
+	const { fields, append, remove } = useFieldArray({
+		name: 'types',
+		control
+	});
+
+	const handleError = (errors) => {
+		console.log(errors);
 	};
 
-	return <div>Boop</div>;
-};
+	function onSubmit(data) {
+		console.log(data);
+		//try {
+		//	socket.emit('request', {
+		//		route: 'gameConfig',
+		//		action: 'create',
+		//		data
+		//	});
+		//	// eslint-disable-next-line no-empty
+		//} catch (err) {}
+	}
+
+	return (
+		<Form onSubmit={handleSubmit(onSubmit, handleError)}>
+			{fields.map((item, i) => (
+				<div key={i} className="list-group list-group-flush">
+					<div className="list-group-item">
+						<div>
+							<Input placeholder=" " type="text" {...register(`types.${i}`)} />
+						</div>
+					</div>
+				</div>
+			))}
+
+			<ButtonGroup>
+				<Button onClick={() => append({ type: '' })}>Add Type</Button>
+				<Button type="submit" className="btn btn-primary mr-1">
+					Create Initial Config
+				</Button>
+				<Button
+					onClick={() => reset()}
+					type="button"
+					className="btn btn-secondary mr-1"
+				>
+					Reset
+				</Button>
+			</ButtonGroup>
+		</Form>
+	);
+}
 
 export default GameConfig;
