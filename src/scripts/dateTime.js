6b27e5ad9652b748c37118066ea83a0823c637d8
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-07-31 21:58:37 +0200

    making the helper function return the local time

--- src/scripts/dateTime.js
+++ src/scripts/dateTime.js
@@ -1,27 +1,16 @@
 export const getDateTimeString = (date) => {
 	const d = new Date(date);
-	const dd = d.getDate()<10 ? '0'+d.getDate() : d.getDate();
-	const mm = d.getMonth()<10 ? '0'+d.getMonth() : d.getMonth();
-	const yyyy = d.getFullYear();
-	const hh = d.getUTCHours()<10 ? '0'+d.getUTCHours() : d.getUTCHours();
-	const min = d.getMinutes()<10 ? '0'+d.getMinutes() : d.getMinutes();
-	const dateTimeString = `${yyyy}/${mm}/${dd} ${hh}:${min} (UTC)`;
-	return dateTimeString;
+	// TODO: get Date/time to local date/time
+	var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+	return d.toLocaleString('en-US', options);
 };
 
 export const getDateString = (date) => {
 	const d = new Date(date);
-	const dd = d.getDate()<10 ? '0'+d.getDate() : d.getDate();
-	const mm = d.getMonth()<10 ? '0'+d.getMonth() : d.getMonth();
-	const yyyy = d.getFullYear();
-	const dateString = `${yyyy}/${mm}/${dd}`;
-	return dateString;
+	return d.toLocaleDateString('en-US');
 };
 
 export const getTimeString = (date) => {
 	const d = new Date(date);
-	const hh = d.getUTCHours()<10 ? '0'+d.getUTCHours() : d.getUTCHours();
-	const min = d.getMinutes()<10 ? '0'+d.getMinutes() : d.getMinutes();
-	const TimeString = `${hh}:${min} (UTC)`;
-	return TimeString;
+	return d.toLocaleTimeString('en-US');
 };
