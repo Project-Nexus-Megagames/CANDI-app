6fc8371ab5cccc916df2e2875f34470b67cc77b7
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-07-31 17:45:35 +0200

    small fix in the helper function

--- src/scripts/dateTime.js
+++ src/scripts/dateTime.js
@@ -1,10 +1,10 @@
 export const getDateTimeString = (date) => {
 	const d = new Date(date);
 	const dd = d.getDate()<10 ? '0'+d.getDate() : d.getDate();
-	const mm = d.getMonth()<10 ? '0'+d.getMonth() : d.getMonth;
+	const mm = d.getMonth()<10 ? '0'+d.getMonth() : d.getMonth();
 	const yyyy = d.getFullYear();
-	const hh = d.getUTCHours();
-	const min = d.getMinutes();
+	const hh = d.getUTCHours()<10 ? '0'+d.getUTCHours() : d.getUTCHours();
+	const min = d.getMinutes()<10 ? '0'+d.getMinutes() : d.getMinutes();
 	const dateTimeString = `${yyyy}/${mm}/${dd} ${hh}:${min} (UTC)`;
 	return dateTimeString;
 };
