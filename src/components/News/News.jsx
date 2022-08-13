481d2ee446fdb91780c327b7ace3618860729ae3
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-08-14 00:10:56 +0200

    generic "newsfeed" component and News page

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -5,7 +5,13 @@ import { Route, Switch, NavLink, Redirect } from 'react-router-dom';
 import NewsFeed from '../Common/NewsFeed';
 import NavigationBar from '../Navigation/NavigationBar';
 
-const News = (props) => {
-	return <NewsFeed />;
+const News = () => {
+	const data = [
+		{ id: '1', avatarUrl: '', name: 'John Doe', title: 'This is a test', date: 'August 8, 2022' },
+		{ id: '2', avatarUrl: '', name: 'John Doe', title: 'This is a test', date: 'Augst 8, 2022' },
+		{ id: '3', avatarUrl: '', name: 'Jane Doe', title: 'This is a test', date: 'August 7, 2022' }
+	];
+
+	return <NewsFeed data={data} />;
 };
 export default News;
---
+++ src/components/common/NewsFeed.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Avatar, Box, HStack, Stack, StackDivider, Text, VStack, Center } from '@chakra-ui/react';
+
+const NewsFeed = (props) => {
+	const data = props.data;
+
+	return (
+		<Center>
+			<Box bg="bg-surface" py="4">
+				<Stack divider={<StackDivider />} spacing="4">
+					{data.map((item) => (
+						<Stack key={item.id} fontSize="sm" px="4" spacing="4" margin="5px">
+							<Stack direction="row" justify="space-between" spacing="4">
+								<HStack spacing="3">
+									<Avatar src={item.avatarUrl} boxSize="10" />
+									<Box>
+										<Text fontWeight="medium" color="emphasized">
+											{item.name}
+										</Text>
+									</Box>
+								</HStack>
+								<Text color="muted">{item.date}</Text>
+							</Stack>
+							<VStack align="left">
+								<Text fontSize="2xl" align="left">
+									{item.title}
+								</Text>
+								<Text
+									color="muted"
+									noOfLines={2}
+									align="left"
+									sx={{
+										'-webkit-box-orient': 'vertical',
+										'-webkit-line-clamp': '2',
+										overflow: 'hidden',
+										display: '-webkit-box'
+									}}
+								>
+									Candy donut tart pudding macaroon. Soufflé carrot cake choc late cake biscuit jelly beans chupa chups dragée. Cupcake toffee gummies lemon drops halvah. Cookie fruitcake jelly beans gingerbread soufflé marshmallow. Candy donut tart pudding macaroon. Soufflé carrot cake choc late cake biscuit jelly beans chupa chups dragée. Cupcake toffee gummies lemon drops halvah. Cookie fruitcake jelly beans gingerbread soufflé marshmallow. Candy donut tart pudding macaroon. Soufflé carrot
+									cake choc late cake biscuit jelly beans chupa chups dragée. Cupcake toffee gummies lemon drops halvah. Cookie fruitcake jelly beans gingerbread soufflé marshmallow. Candy donut tart pudding macaroon. Soufflé carrot cake choc late cake biscuit jelly beans chupa chups dragée. Cupcake toffee gummies lemon drops halvah. Cookie fruitcake jelly beans gingerbread soufflé marshmallow. Candy donut tart pudding macaroon. Soufflé carrot cake choc late cake biscuit jelly beans chupa chups
+									dragée. Cupcake toffee gummies lemon drops halvah. Cookie fruitcake jelly beans gingerbread soufflé marshmallow. Candy donut tart pudding macaroon. Soufflé carrot cake choc late cake biscuit jelly beans chupa chups dragée. Cupcake toffee gummies lemon drops halvah. Cookie fruitcake jelly beans gingerbread soufflé marshmallow.
+								</Text>
+							</VStack>
+						</Stack>
+					))}
+				</Stack>
+			</Box>
+		</Center>
+	);
+};
+
+export default NewsFeed;
