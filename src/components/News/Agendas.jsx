5c91dd6f84e6885ed793a017c306aa9d8744a3c0
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-08-21 23:00:52 +0200

    adding search box in the right place

--- src/components/News/Agendas.jsx
+++ src/components/News/Agendas.jsx
@@ -1,9 +1,10 @@
-import React from 'react'; // React import
+import React, { useState } from 'react'; // React import
 import NewsFeed from '../Common/NewsFeed';
 import NavigationBar from '../Navigation/NavigationBar';
 import { useSelector } from 'react-redux';
 import { Heading } from '@chakra-ui/react';
 import { getAgendaActions } from '../../redux/entities/playerActions';
+import { Stack, Input } from '@chakra-ui/react';
 import { Loader } from 'rsuite';
 
 const Agendas = (props) => {
@@ -23,12 +24,22 @@ const Agendas = (props) => {
 	});
 
 	const data = agendas.map((el) => ({ author: el.creator.characterName, authorProfilePicture: el.creator.profilePicture, title: el.name, body: el.submission?.description, date: el.updatedAt, comments: el.comments, authorId: el.creator._id, articleId: el._id, type: 'agenda' }));
+	const [filteredData, setFilteredData] = useState(data);
+
+	const handleSearch = (e) => {
+		let filtered = [];
+		filtered = data.filter((article) => article.title.toLowerCase().includes(e) || article.body?.toLowerCase().includes(e));
+		setFilteredData(filtered);
+	};
 
 	return (
 		<React.Fragment>
 			<NavigationBar />
-			<Heading>Agendas</Heading>
-			<NewsFeed data={data} />
+			<Stack align="center" spacing="4">
+				<Heading>Agendas</Heading>
+				<Input style={{ width: '30%' }} placeholder="Search" onChange={(e) => handleSearch(e.target.value)}></Input>
+			</Stack>
+			<NewsFeed data={filteredData} />
 		</React.Fragment>
 	);
 };
