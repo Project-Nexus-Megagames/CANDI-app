a4c96cdbb6ae9813b6e2e2d6ab2304d23e471a8a
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-08-22 19:11:56 +0200

    feat: :sparkles: carousel box is now clickable

--- src/components/Common/Carousel.jsx
+++ src/components/Common/Carousel.jsx
@@ -64,28 +64,32 @@ const Carousel = (props) => {
 		>
 			{/* Left Icon */}
 			<IconButton aria-label="left-arrow" variant="ghost" position="absolute" left={side} top={top} transform={'translate(0%, -50%)'} zIndex={2} onClick={() => slider?.slickPrev()}>
-				<BiLeftArrowAlt size="40px" />
+				<BiLeftArrowAlt size="40px" color="GrayText" />
 			</IconButton>
 			{/* Right Icon */}
 			<IconButton aria-label="right-arrow" variant="ghost" position="absolute" right={side} top={top} transform={'translate(0%, -50%)'} zIndex={2} onClick={() => slider?.slickNext()}>
-				<BiRightArrowAlt size="40px" />
+				<BiRightArrowAlt size="40px" color="GrayText" />
 			</IconButton>
 			{/* Slider */}
-			<Slider {...settings} ref={(slider) => setSlider(slider)}>
-				{cards.map((card, index) => (
-					<Box key={index} height={'6xl'} position="relative" backgroundPosition="center" backgroundRepeat="no-repeat" backgroundSize="cover" backgroundImage={`url(${card.image})`}>
-						{/* This is the block you need to change, to customize the caption */}
-						<Container size="container.lg" height={props.height} position="relative">
-							<Stack spacing={6} w={'full'} maxW={'lg'} position="absolute" top="50%" transform="translate(0, -50%)">
-								<Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>{card.title}</Heading>
-								<Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText" noOfLines={2}>
-									{card.text}
-								</Text>
-							</Stack>
-						</Container>
-					</Box>
-				))}
-			</Slider>
+			<Link to={props.disabled ? '#' : props.to}>
+				<Slider {...settings} ref={(slider) => setSlider(slider)}>
+					{cards.map((card, index) => (
+						<Box key={index} height={'6xl'} position="relative" backgroundPosition="center" backgroundRepeat="no-repeat" backgroundSize="cover" backgroundImage={`url(${card.image})`}>
+							{/* This is the block you need to change, to customize the caption */}
+							<Container size="container.lg" height={props.height} position="relative">
+								<Stack spacing={6} w={'full'} maxW={'lg'} position="absolute" top="50%" transform="translate(0, -50%)">
+									<Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }} color="#0f131a">
+										{card.title}
+									</Heading>
+									<Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText" noOfLines={2}>
+										{card.text}
+									</Text>
+								</Stack>
+							</Container>
+						</Box>
+					))}
+				</Slider>{' '}
+			</Link>
 			<div>
 				<Link to={props.to}>
 					<h6
