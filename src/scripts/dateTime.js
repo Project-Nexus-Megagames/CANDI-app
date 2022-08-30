e2a8e046c8b12fba933f8407e47cc8ffd19ae3ba
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-08-31 00:06:58 +0200

    chore: :broom: cleanUp TODO comments

--- src/scripts/dateTime.js
+++ src/scripts/dateTime.js
@@ -1,6 +1,6 @@
 export const getDateTimeString = (date) => {
 	const d = new Date(date);
-	// TODO: get Date/time to local date/time
+
 	var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
 	return d.toLocaleString('en-US', options);
 };
--- src/services/uploads.js
+++ src/services/uploads.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const API_URL = "http://localhost:5000"
 
-// TODO: create 3 different const for small, medium, larg
 const cloudinaryUpload = (fileToUpload) => {
     return axios.post(API_URL + '/api/imageUpload', fileToUpload)
     .then(res => res.data)
