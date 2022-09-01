bcd5b1ab58064ffb2712fc43728deab23e776143
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-09-01 17:34:21 +0200

    that was a stupid typo

--- src/services/uploads.js
+++ src/services/uploads.js
@@ -1,28 +1,27 @@
 import axios from 'axios'
-const API_URL = "http://localhost:5000"
 import { gameServer } from "../config";
 
 
 const cloudinaryUpload = (fileToUpload) => {
-    return axios.post(API_URL + `${gameServer}api/imageUpload`, fileToUpload)
+    return axios.post(`${gameServer}api/imageUpload`, fileToUpload)
     .then(res => res.data)
     .catch(err => console.log(err))
 }
 
 const cloudinaryUploadSmall = (fileToUpload) => {
-	return axios.post(API_URL + `${gameServer}api/imageUpload/small`, fileToUpload)
+	return axios.post(`${gameServer}api/imageUpload/small`, fileToUpload)
 	.then(res => res.data)
 	.catch(err => console.log(err))
 }
 
 const cloudinaryUploadMedium = (fileToUpload) => {
-	return axios.post(API_URL + `${gameServer}api/imageUpload/medium`, fileToUpload)
+	return axios.post(`${gameServer}api/imageUpload/medium`, fileToUpload)
 	.then(res => res.data)
 	.catch(err => console.log(err))
 }
 
 const cloudinaryUploadLarge = (fileToUpload) => {
-	return axios.post(API_URL + `${gameServer}api/imageUpload/large`, fileToUpload)
+	return axios.post(`${gameServer}api/imageUpload/large`, fileToUpload)
 	.then(res => res.data)
 	.catch(err => console.log(err))
 }
