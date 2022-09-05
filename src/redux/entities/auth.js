29c0ab21482d44da0fe1468c64b58cb6bc6b61c5
franziskafschulz <franziska.f.schulz@gmail.com>
2022-09-05 11:46:03 +0200

    fix: :ambulance: fix auth control

--- src/redux/entities/auth.js
+++ src/redux/entities/auth.js
@@ -1,8 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"; // Import from reactjs toolkit
 import { apiCallBegan } from "../api"; // Import Redux API call
 // import playTrack from "../../scripts/audio";
-import jwtDecode from 'jwt-decode' // JSON web-token decoder
-
+import jwtDecode from "jwt-decode"; // JSON web-token decoder
 
 // Create entity slice of the store
 const slice = createSlice({
@@ -15,10 +14,11 @@ const slice = createSlice({
     lastLogin: null,
     control: false,
     users: [],
-    error: null
+    error: null,
   },
   // Reducers - Events
-  reducers: { // this will become hadleLogin from app.js
+  reducers: {
+    // this will become hadleLogin from app.js
     loginRequested: (auth, action) => {
       console.log(`${action.type} Dispatched...`);
       auth.loading = true;
@@ -27,28 +27,27 @@ const slice = createSlice({
       console.log(`${action.type} Dispatched...`);
 
       let jwt = action.payload.token;
-      localStorage.setItem('candi-token', jwt );
+      localStorage.setItem("candi-token", jwt);
       const user = jwtDecode(jwt);
-      console.log(localStorage)
+      console.log(localStorage);
 
-      if (user.roles.some(el => el === "Control")) auth.control = true;
+      //if (user.roles.some(el => el === "Control")) auth.control = true;
 
-			auth.error = null;
+      auth.error = null;
       auth.user = user;
       auth.lastLogin = Date.now();
-      auth.loading = false
-			auth.login = true;
-	
+      auth.loading = false;
+      auth.login = true;
     },
     authRequestFailed: (auth, action) => {
-      console.log(`${action.type} Dispatched`)
-			auth.loading = false;
+      console.log(`${action.type} Dispatched`);
+      auth.loading = false;
       auth.error = action.payload;
     },
     loginSocket: (auth, action) => {
       console.log(`${action.type} Dispatched`);
       auth.socket = action.payload.me;
-		},
+    },
     finishLoading: (auth, action) => {
       console.log(`${action.type} Dispatched`);
       auth.loadComplete = true;
@@ -56,50 +55,38 @@ const slice = createSlice({
     setCharacter: (auth, action) => {
       console.log(`${action.type} Dispatched`);
       auth.character = action.payload;
-      if (action.payload.tags.some(el => el.toLowerCase() === "control")) auth.control = true;
+      if (action.payload.tags.some((el) => el.toLowerCase() === "control")) auth.control = true;
       // initConnection(auth.user, auth.team, auth.version);
     },
     setControl: (auth, action) => {
       auth.control = action.payload.control;
     },
-		clearAuthError: (auth, action) => {
-			console.log(`${action.type} Dispatched`);
-			auth.error = null;
-		},
-		signOut: (auth, action) => {
+    clearAuthError: (auth, action) => {
       console.log(`${action.type} Dispatched`);
-      localStorage.removeItem('candi-token');
-			auth.user = null;
-			auth.login = false;
-			auth.loading = false;
-			auth.lastLogin = null;
-			auth.error = null;
-		},
-    usersRecieved: (auth, action) => {
-      console.log(`${action.type} Dispatched`)
-      auth.users = action.payload
+      auth.error = null;
     },
-		updateUser: (auth, action) => {
-			console.log(`${action.type} Dispatched`);
-			auth.user = action.payload.user;
-		}
-  }
+    signOut: (auth, action) => {
+      console.log(`${action.type} Dispatched`);
+      localStorage.removeItem("candi-token");
+      auth.user = null;
+      auth.login = false;
+      auth.loading = false;
+      auth.lastLogin = null;
+      auth.error = null;
+    },
+    usersRecieved: (auth, action) => {
+      console.log(`${action.type} Dispatched`);
+      auth.users = action.payload;
+    },
+    updateUser: (auth, action) => {
+      console.log(`${action.type} Dispatched`);
+      auth.user = action.payload.user;
+    },
+  },
 });
 
 // Action Export
-export const {
-  authReceived,
-  loginRequested,
-  authRequestFailed,
-	loginSocket,
-	clearAuthError,
-	signOut,
-	updateUser,
-  usersRecieved, 
-  finishLoading,
-  setCharacter,
-  setControl
-} = slice.actions;
+export const { authReceived, loginRequested, authRequestFailed, loginSocket, clearAuthError, signOut, updateUser, usersRecieved, finishLoading, setCharacter, setControl } = slice.actions;
 
 export default slice.reducer; // Reducer Export
 
@@ -107,15 +94,15 @@ export default slice.reducer; // Reducer Export
 const url = "https://nexus-central-server.herokuapp.com/auth";
 
 // aircraft Loader into state
-export const loginUser = payload => (dispatch, getState) => {
+export const loginUser = (payload) => (dispatch, getState) => {
   return dispatch(
     apiCallBegan({
       url,
-      method: 'post',
+      method: "post",
       data: payload,
-      onStart:loginRequested.type,
-      onSuccess:authReceived.type,
-      onError:authRequestFailed.type
+      onStart: loginRequested.type,
+      onSuccess: authReceived.type,
+      onError: authRequestFailed.type,
     })
   );
 };
