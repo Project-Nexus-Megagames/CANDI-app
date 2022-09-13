bcc4b13e5c118bd455bd464c72fb3cbe27be80c0
Scott McMann <smcmann42@gmail.com>
2022-09-13 13:10:46 -0700

    update version

--- src/components/Common/ArticleAlert.jsx
+++ src/components/Common/ArticleAlert.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
-import { AlertDescription, AlertIcon, AlertTitle, Alert, Image, } from '@chakra-ui/react';
+import React, { useEffect, useState } from "react";
+import { AlertDescription, AlertIcon, AlertTitle, Alert, Image, CloseButton, } from '@chakra-ui/react';
 
 export const ArticleAlert = (props) => {
   const article = props.data;
+	const [show, setShow] = useState(true);
+
 
   useEffect(() => {
     const audio = new Audio("/skullsound2.mp3");
@@ -13,18 +15,29 @@ export const ArticleAlert = (props) => {
 
 
   return (
-    <Alert
-    style={{ backgroundColor: '#15181e', borderRadius: '10px' }}
-    status='success'
-    flexDirection='column'
-    alignItems='center'
-    justifyContent='center'
-    textAlign='center'
-    height='300px'
-  >
-    <h5>New Article Posted!</h5>
-    {article.image && <Image src={article.image} width="100%" height={'20vh'} fit='cover' />}
-    <h3>      {article.title}    </h3>
-  </Alert>
+    <div>
+      {show && <Alert
+      style={{ backgroundColor: '#15181e', borderRadius: '10px' }}
+      status='success'
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='center'
+      textAlign='center'
+      height='300px'
+    >
+      <h5>New Article Posted!</h5>
+      <CloseButton
+          alignSelf='flex-end'
+          position='relative'
+          right={10}
+          top={-20}
+          onClick={() => setShow(false)}
+        />
+      {article.image && <Image src={article.image} width="100%" height={'15vh'} fit='cover' />}
+      <h3>      {article.title}    </h3>
+      
+    </Alert>}      
+    </div>
+
   );
 };
