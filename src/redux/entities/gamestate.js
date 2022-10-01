50c164e7a93011c793fadf4ba949c32a4656a899
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-10-02 00:17:13 +0200

    version

--- src/redux/entities/gamestate.js
+++ src/redux/entities/gamestate.js
@@ -6,7 +6,7 @@ import { apiCallBegan } from "../api"; // Import Redux API call
 const slice = createSlice({
   name: "gamestate",
 	initialState: {
-    version: '1.6.6',
+    version: '1.7.0',
     loading: false,
     loaded: false,
     lastFetch: null,
