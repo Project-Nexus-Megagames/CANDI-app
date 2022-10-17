bec3493d139ea017e04e8969d447a08065722cb7
pattonicole <pattonicole@gmail.com>
2022-10-17 14:33:51 -0500

    update action drawer to be mobile friendly

--- src/components/Actions/ActionList/ActionSidebar.jsx
+++ src/components/Actions/ActionList/ActionSidebar.jsx
@@ -1,62 +1,125 @@
 import React from "react";
-import { Button, Icon, Input, InputGroup, PanelGroup, Sidebar, Tooltip, Whisper } from "rsuite";
 import ActionList from "./ActionList";
+import {
+    Center,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    Flex,
+    IconButton,
+    Input, InputGroup, InputLeftElement,
+    Tooltip,
+    useBreakpointValue,
+    useDisclosure
+} from "@chakra-ui/react";
+import { AddIcon, SearchIcon } from '@chakra-ui/icons'
+
+/*
+function DrawerExample() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+  return (
+    <>
+      <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue'>Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+ */
 
 function ActionSidebar(props) {
-    return (
-        <Sidebar>
-            <PanelGroup>
-                <div
-                    style={{
-                        height: "40px",
-                        borderRadius: "0px",
-                        backgroundColor: "#000101"
-                    }}
-                >
-                    <InputGroup>
-                        <Input
-                            size="lg"
-                            style={{height: "42px"}}
-                            onChange={props.onChange}
-                            value={props.value}
-                            placeholder="Search"></Input>
+    const {isOpen, onOpen, onClose} = useDisclosure({defaultIsOpen: true});
+    const drawerSize = useBreakpointValue({base: 'full', sm: 'sm'})
 
-                        <Whisper
-                            placement="top"
-                            trigger="hover"
-                            speaker={
-                                <Tooltip>
-                                    <b>{`Create New Action`}</b>
-                                </Tooltip>
-                            }
+    return (
+        <Drawer
+            isOpen={isOpen}
+            placement='left'
+            onClose={onClose}
+            isFullHeight
+            size={drawerSize}
+        >
+            <DrawerOverlay/>
+            <DrawerContent
+                style={{
+                    backgroundColor: "#0f131a"
+                }}
+            >
+                <DrawerCloseButton/>
+                <DrawerHeader>
+                    <Center>
+                        Action List
+                    </Center>
+                    <Flex>
+                        <InputGroup>
+                            <InputLeftElement
+                                pointerEvents='none'
+                            >
+                                <SearchIcon/>
+                            </InputLeftElement>
+                            <Input
+                                onChange={props.onChange}
+                                value={props.value}
+                                placeholder="Search"
+                                color='white'
+                            />
+                        </InputGroup>
+                        <Tooltip
+                            label='Add New Action'
+                            aria-label='a tooltip'
                         >
-                            <Button
-                                style={{color: "black", borderRadius: "0px"}}
-                                color="green"
-                                onClick={props.onClick}>
-                                <Icon icon="plus"/>
-                            </Button>
-                        </Whisper>
-                    </InputGroup>
-                </div>
-                <div
-                    style={{
-                        height: "calc(100vh - 80px)",
-                        overflow: "auto",
-                        borderRadius: "0px",
-                        borderRight: "1px solid rgba(255, 255, 255, 0.12)"
-                    }}
-                >
+                            <IconButton
+                                icon={<AddIcon/>}
+                                onPress={props.onClick}
+                                colorScheme={'green'}
+                                style={{
+                                    marginLeft: '1rem'
+                                }}
+                                aria-label='Add New Action'
+                            />
+                        </Tooltip>
+                    </Flex>
+                </DrawerHeader>
+                <DrawerBody>
                     <ActionList
                         actions={props.control ? props.filteredActions : props.myActions}
                         actionTypes={props.actionTypes}
                         selected={props.selected}
                         handleSelect={props.handleSelect}
                     />
-                </div>
-                ActionList
-            </PanelGroup>
-        </Sidebar>
+                </DrawerBody>
+                <DrawerFooter/>
+            </DrawerContent>
+        </Drawer>
     );
 }
 
