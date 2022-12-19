b71aef392c0730e94c1350465bb49e06e1a96793
Scott McMann <smcmann@gmail.com>
2022-12-18 20:15:18 -0800

    update server env

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let gameServer = 'http://localhost:5000/';
 
 if (process.env.NODE_ENV === "production") {
-	gameServer = 'https://candi-server.herokuapp.com/';
+	gameServer = 'https://godswar-server.onrender.com/';
 	// console.log(process.env)
 }
 
