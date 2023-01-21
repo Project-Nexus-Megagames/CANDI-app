2dd0a0dbe9863e4379b2e084c6f3dd0eb963fd1a
Scott McMann <smcmann@gmail.com>
2023-01-21 10:42:15 -0800

    add hash

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./fonts/BloodyTerror.ttf";
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<>
 		<ChakraProvider>
-			<BrowserRouter>
+			<HashRouter>
 				<Provider store={store}>
 					<App />
 				</Provider>
-			</BrowserRouter>
+			</HashRouter>
 		</ChakraProvider>
 	</>
 );
