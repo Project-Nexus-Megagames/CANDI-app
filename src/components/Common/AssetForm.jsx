ec6348da89732de03a734b13e330ed7dfb756675
Franzi <34717874+franziskas1981@users.noreply.github.com>
2023-02-04 22:58:53 +0100

    get resource type from config for new resources

--- src/components/Common/AssetForm.jsx
+++ src/components/Common/AssetForm.jsx
@@ -9,13 +9,13 @@ import SelectPicker from './SelectPicker';
 import socket from '../../socket';
 
 const AssetForm = (props) => {
-  const { asset, character, mode } = props;
+	const { asset, character, mode } = props;
 	const loggedInUser = useSelector((state) => state.auth.user);
-
+	const gameConfig = useSelector((state) => state.gameConfig);
 	const [imageURL, setImageURL] = useState('');
 	const [type, setType] = useState(asset ? asset.type : 'Asset');
-	const [status, setStatus] = useState((asset && asset.status) ? asset.status : []);
-
+	const [status, setStatus] = useState(asset && asset.status ? asset.status : []);
+	console.log(gameConfig.resourceTypes);
 	const { register, control, handleSubmit, reset, formState, watch } = useForm(
 		{
 			defaultValues: asset
@@ -65,29 +65,27 @@ const AssetForm = (props) => {
 	};
 
 	const handleStatus = (stuff) => {
-    const stat = stuff.target.id;
+		const stat = stuff.target.id;
 
-    if (status.some(el => el === stat)) {
-      let temp = status.filter(el0 => el0 !== stat);
-  		setStatus(temp)    
-    }
-    else setStatus([...status, stat])
+		if (status.some((el) => el === stat)) {
+			let temp = status.filter((el0) => el0 !== stat);
+			setStatus(temp);
+		} else setStatus([...status, stat]);
 	};
 
 	function onSubmit(data, e) {
-    if (props.handleSubmit) {
-      props.handleSubmit(data)
-    }
-    else {
-      e.preventDefault();
-      const asset = { ...data, "type": type, "status": status, ownerCharacter: props.character._id }
-      console.log('SENDING DATA', asset);
-      socket.emit('request', {
-        route: 'asset',
-        action: mode,
-        data: { asset, imageURL, loggedInUser }
-      });      
-    }
+		if (props.handleSubmit) {
+			props.handleSubmit(data);
+		} else {
+			e.preventDefault();
+			const asset = { ...data, type: type, status: status, ownerCharacter: props.character._id };
+			console.log('SENDING DATA', asset);
+			socket.emit('request', {
+				route: 'asset',
+				action: mode,
+				data: { asset, imageURL, loggedInUser }
+			});
+		}
 		props.closeModal();
 	}
 
@@ -95,65 +93,58 @@ const AssetForm = (props) => {
 		console.log('ERROR', errors);
 	};
 
-  const assetTypes = [ { name: 'Asset'}, { name: 'Trait' }, { name: 'Power' } ];
+	//const assetTypes = [ { name: 'Asset'}, { name: 'Trait' }, { name: 'Power' } ];
 
 	return (
-    <form onSubmit={handleSubmit(onSubmit, handleError)}>
-      <Box>
-              <VStack spacing="24px" w="100%">
-                  <Flex >
-                  <Spacer />
-                  <FormControl>
-                    <FormLabel>Type </FormLabel>
-                    {/* <Input type="text" size="md" variant="outline" {...register('type', validation.type)}></Input> setValue('test', '')  */} 
-                    <SelectPicker label={'name'} data={assetTypes} onChange={(ddd) => setType(ddd) } value={type}/>
-                  </FormControl>
-                  <Spacer />
-                  </Flex>
+		<form onSubmit={handleSubmit(onSubmit, handleError)}>
+			<Box>
+				<VStack spacing='24px' w='100%'>
+					<Flex>
+						<Spacer />
+						<FormControl>
+							<FormLabel>Type </FormLabel>
+							{/* <Input type="text" size="md" variant="outline" {...register('type', validation.type)}></Input> setValue('test', '')  */}
+							<SelectPicker label={'type'} data={gameConfig.resourceTypes} onChange={(ddd) => setType(ddd)} value={type} />
+						</FormControl>
+						<Spacer />
+					</Flex>
 
-                  <FormControl>
-                      <FormLabel>Asset Name </FormLabel>
-                      <Input type="text" size="md" variant="outline" {...register('name', validation.name)}></Input>
-                      <Text fontSize="sm" color="red.500">
-                        {errors.name && errors.name.message}
-                      </Text>
-                    </FormControl>
+					<FormControl>
+						<FormLabel>Asset Name </FormLabel>
+						<Input type='text' size='md' variant='outline' {...register('name', validation.name)}></Input>
+						<Text fontSize='sm' color='red.500'>
+							{errors.name && errors.name.message}
+						</Text>
+					</FormControl>
 
-                  <FormControl>
-                    <FormLabel>Description </FormLabel>
-                    <Input type="text" size="md" variant="outline" {...register('description', validation.description)}></Input>
-                    <Text fontSize="sm" color="red.500">
-                      {errors.description && errors.description.message}
-                    </Text>
-                  </FormControl>
+					<FormControl>
+						<FormLabel>Description </FormLabel>
+						<Input type='text' size='md' variant='outline' {...register('description', validation.description)}></Input>
+						<Text fontSize='sm' color='red.500'>
+							{errors.description && errors.description.message}
+						</Text>
+					</FormControl>
 
-                <Grid templateColumns={`repeat(2, 1fr)`} width={'100%'}>
-                  {['hidden', 'lent', 'lendable', 'used'].map( stat => (
-                    <FormControl key={stat} display='flex' alignItems='center'>
-                      <FormLabel mb='0'>
-                        {stat}
-                      </FormLabel>
-                      <Switch id={stat} onChange={handleStatus} isChecked={status.some(el => el === stat)} />
-                    </FormControl>                    
-                  ))}                  
-                </Grid>
+					<Grid templateColumns={`repeat(2, 1fr)`} width={'100%'}>
+						{['hidden', 'lent', 'lendable', 'used'].map((stat) => (
+							<FormControl key={stat} display='flex' alignItems='center'>
+								<FormLabel mb='0'>{stat}</FormLabel>
+								<Switch id={stat} onChange={handleStatus} isChecked={status.some((el) => el === stat)} />
+							</FormControl>
+						))}
+					</Grid>
+				</VStack>
+			</Box>
 
-
-
-
-              </VStack>
-      </Box>
-      
-      <ButtonGroup>
-        <Button type="submit" colorScheme="teal" disabled={type === ''} className="btn btn-primary mr-1">
-          Create Asset
-        </Button>
-        <Button colorScheme={'yellow'} onClick={() => reset()} leftIcon={<RepeatClockIcon />} >
-          Reset Form
-        </Button>
-
-      </ButtonGroup>
-    </form>   
+			<ButtonGroup>
+				<Button type='submit' colorScheme='teal' disabled={type === ''} className='btn btn-primary mr-1'>
+					Create Asset
+				</Button>
+				<Button colorScheme={'yellow'} onClick={() => reset()} leftIcon={<RepeatClockIcon />}>
+					Reset Form
+				</Button>
+			</ButtonGroup>
+		</form>
 	);
 };
 
