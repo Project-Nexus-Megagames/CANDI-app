1ba2dde273aace9af07eb19ae5e75b326d250bc7
Franzi <34717874+franziskas1981@users.noreply.github.com>
2023-02-10 08:54:43 +0100

    fix SelectPicker component

--- src/components/Common/SelectPicker.jsx
+++ src/components/Common/SelectPicker.jsx
@@ -4,16 +4,24 @@ import { IoChevronDownCircleOutline } from 'react-icons/io5';
 import { getFadedColor } from '../../scripts/frontend';
 
 function SelectPicker(props) {
-  const { data, value, onChange, label, valueKey } = props;
-  let [filter, setFilter] = React.useState('');
-  return (
-    <Select style={{ border: `4px solid ${getFadedColor(value)}`, minWidth: '10vw' }} icon={<IoChevronDownCircleOutline />} placeholder={value ? value : 'Select Option'}>
-      {data && data
-      .filter(el => el[label]?.toLowerCase().includes(filter.toLowerCase()))
-      .map(el => (
-        <option style={{ background: "#2d3748", color: 'white' }} onClick={() => onChange(el[valueKey])} key={el[label]} value={el[label]} >{el[label]}</option>
-      ))}
-    </Select>
+	const { data, value, onChange, label, valueKey } = props;
+	let [filter, setFilter] = React.useState('');
+	return (
+		<Select
+			style={{ border: `4px solid ${getFadedColor(value)}`, minWidth: '10vw' }}
+			onChange={() => onChange(event.target.value)}
+			icon={<IoChevronDownCircleOutline />}
+			placeholder={value ? value : 'Select Option'}
+		>
+			{data &&
+				data
+					.filter((el) => el[label]?.toLowerCase().includes(filter.toLowerCase()))
+					.map((el) => (
+						<option style={{ background: '#2d3748', color: 'white' }} key={el[label]} value={el[label]}>
+							{el[label]}
+						</option>
+					))}
+		</Select>
 	);
 }
 
