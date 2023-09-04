003e81a68d4da40e1e6abf89a1ad3d8bf6c74208
Scott McMann <57707915+SMcMann@users.noreply.github.com>
2023-09-04 14:13:46 -0700

    fix leaderboard

--- src/components/StatDisplays/CharacterStats.jsx
+++ src/components/StatDisplays/CharacterStats.jsx
@@ -4,6 +4,7 @@ import { connect, useSelector } from 'react-redux';
 import { getGodBonds, getMortalBonds } from '../../redux/entities/assets';
 import { Box, Button, Flex, HStack, SimpleGrid, Spacer, Spinner, Stack, VStack } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import CharacterNugget from '../Common/CharacterNugget';
 
 const  CharacterStats = (props) => {
 	const config = useSelector((state) => state.gameConfig);
@@ -22,11 +23,15 @@ const  CharacterStats = (props) => {
             <div key={stat._id} style={{ border: '3px solid pink', borderRadius: '5px', width: '100%', }} >
               <h5 style={{ backgroundColor: 'pink', color: 'black' }}>{stat.type}</h5>
               <VStack hover size="sm">
-                {props.characters.filter(el => el.tags.some(tag => tag.toLowerCase() === 'pc')).sort(function(a, b){return b[stat.type] - a[stat.type]}).splice(0, number).map((character, index) => (
+                {props.characters.filter(el => el.tags.some(tag => tag.toLowerCase() === 'pc')).sort(function(a, b){return b.characterStats.find(el => el.type === stat.type)?.statAmount - a.characterStats.find(el => el.type === stat.type)?.statAmount}).splice(0, number).map((character, index) => (
                 <Box key={index} index={index}>
                 <HStack >
                   <Box colspan={2}>
                     <b>{index+1}</b>{index === 0 ? <b>st</b> : index === 1 ? <b>nd</b> : <b>rd</b>}
+                  </Box>
+
+                  <Box colspan={2}>
+                    <CharacterNugget character={character} />
                   </Box>
 
                   <Spacer />
