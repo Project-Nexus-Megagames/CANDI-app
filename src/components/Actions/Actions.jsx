9630980f07a6a6a0412f74c364a25d2253179307
Scott McMann <57707915+SMcMann@users.noreply.github.com>
2023-10-18 13:26:09 -0700

    undoing the pub actions

--- src/components/Actions/Actions.jsx
+++ src/components/Actions/Actions.jsx
@@ -19,8 +19,7 @@ const Actions = (props) => {
     const [showNewActionModal, setShowNewActionModal] = useState(false);
     const [assetInfo, setAssetInfo] = useState({show: false, asset: ''});
     const [editAction, setEditAction] = useState({show: false, action: null})
-    // const {isControl} = usePermissions();
-    const isControl = true;
+    const {isControl} = usePermissions();
     const [rounds, setRounds] = useState([]);
     const [renderRounds, setRenderRounds] = useState([]);
     const [number, setNumber] = useState(4);
