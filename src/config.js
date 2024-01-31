ad931b72e43c5ea69249ee7156a3b3505af278a2
Scott McMann <57707915+SMcMann@users.noreply.github.com>
2024-01-30 18:36:13 -0800

    server ide

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let gameServer = 'http://localhost:5000/';
 
 if (process.env.NODE_ENV === "production") {
-	gameServer = 'https://goblin-city-server.onrender.com/';
+	gameServer = 'https://goblin-city-server-x49d.onrender.com//';
 	// console.log(process.env)
 }
 
