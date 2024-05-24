6a2e34729d88bc919e32b870f3858d029aefa4c0
Scott McMann <57707915+SMcMann@users.noreply.github.com>
2024-05-24 12:23:17 -0700

    update v

--- src/redux/entities/gamestate.js
+++ src/redux/entities/gamestate.js
@@ -6,7 +6,7 @@ import { apiCallBegan } from '../api'; // Import Redux API call
 const slice = createSlice({
 	name: 'gamestate',
 	initialState: {
-		version: '5.6.7',
+		version: '5.6.8',
 		loading: false,
 		loaded: false,
 		lastFetch: null,
