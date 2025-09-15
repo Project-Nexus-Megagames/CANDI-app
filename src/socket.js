344b3b2eec57bf1d8872a883cdbbef2dda6f3877
Scott McMann <smcmann42@gmail.com>
2025-09-14 20:00:43 -0700

    force socket to be websocket

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,11 @@ import { gameServer } from './config';
 // import store from "./redux/store";
 
 const URL = gameServer;
-const socket = io(URL, { autoConnect: false });
+const socket = io(URL, {
+  autoConnect: false,
+  transports: ["websocket"],
+  withCredentials: true
+});
 
 // DEBUG event showing any event thrown over the socket in console
 // socket.onAny((event, ...args) => {
@@ -12,8 +16,8 @@ const socket = io(URL, { autoConnect: false });
 
 export function initConnection(user, character, version) {
   console.log('Socket Connecting....')
-  socket.auth = { username: user.username, character: character ? character.characterName : "Unassigned" , version }
-    
+  socket.auth = { username: user.username, character: character ? character.characterName : "Unassigned", version }
+
   console.log(socket, URL);
   socket.connect();
   console.log(socket);
